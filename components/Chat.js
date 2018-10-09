eae429e1b9ecc82e35c27f48e2c48800b596bdf1
James Dowell <james.s.dowell@gmail.com>
2018-10-09 15:21:35 -0700

    linked chat component up with firebase

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
+import Fire from '../Fire';
 
-class Chat extends Component {
+type Props = {
+  name?: string,
+};
+
+class Chat extends React.Component<Props> {
 
   static navigationOptions = ({ navigation }) => ({
     title: (navigation.state.params || {}).name || 'Chat!',
@@ -12,15 +16,36 @@ class Chat extends Component {
     messages: [],
   }
 
+  // Start looking for messages. Get messages and add them
+  componentDidMount() {
+    Fire.shared.on(message => {
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message)
+      }))
+    });
+  }
+
+  // Unsubscribing from the db
+  componentWillUnmount() {
+    Fire.shared.off();
+  }
+
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid
+    }
+  }
   render() {
     return (
       <GiftedChat
         messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
       />
     )
   }
 }
 
-const styles = StyleSheet.create({});
 
 export default Chat;
