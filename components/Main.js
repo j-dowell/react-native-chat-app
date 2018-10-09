724194a78fe2d3cbdf715bfb3b30596016993721
James Dowell <james.s.dowell@gmail.com>
2018-10-09 14:19:53 -0700

    added button navigation to chat component

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { View, StyleSheet, TextInput, Text, TouchableOpacity } from 'react-native';
 
 class Main extends Component {
   constructor(props) {
@@ -13,15 +13,23 @@ class Main extends Component {
       name
     })
   }
+
+  onPress = () => {
+    this.props.navigation.navigate('Chat', { name: this.state.name })
+  }
   render() {
     return (
       <View>
+        <Text style={styles.title}>Enter your name:</Text>
         <TextInput
           style={styles.nameInput}
           placeHolder="James"
           value={this.state.name}
           onChangeText={this.onChangeText}
         />
+        <TouchableOpacity onPress={this.onPress}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -35,6 +43,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: offset,
     borderColor: '#111111',
     borderWidth: 1,
+  },
+  title: { // 4.
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  buttonText: { // 5.
+    marginLeft: offset,
+    fontSize: offset,
   },
 });
 
