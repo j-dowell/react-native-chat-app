0929d7f871149614634c0dd61566170cdd4d9cda
James Dowell <james.s.dowell@gmail.com>
2018-10-09 14:15:18 -0700

    added text input

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
 
 class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
+  }
+  onChangeText = name => {
+    this.setState({
+      name
+    })
+  }
   render() {
-    return <View />;
+    return (
+      <View>
+        <TextInput
+          style={styles.nameInput}
+          placeHolder="James"
+          value={this.state.name}
+          onChangeText={this.onChangeText}
+        />
+      </View>
+    )
   }
 }
 
-const styles = StyleSheet.create({});
+const offset = 24;
+const styles = StyleSheet.create({
+  nameInput: { 
+    height: offset * 2,
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+  },
+});
 
 export default Main;
